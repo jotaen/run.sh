729646469f48c45bf21abc92279daa29f80112c1
Jan Heuermann <jan@jotaen.net>
2022-01-26 11:49:38 +0100

    Setup continuous integration

--- run.sh
+++ run.sh
@@ -2,18 +2,21 @@
 
 # Check code style with shellcheck.
 run_lint() {
-	shellcheck run run.sh spec.sh
+	docker run --rm -t \
+		-v "${PWD}:/mnt:ro" \
+		koalaman/shellcheck:stable run run.sh spec.sh
 }
 
 # Run all tests.
 run_test() {
-	docker run -it \
+	docker run --rm -t \
 		-v "${PWD}:/app:ro" \
 		bats/bats:latest /app/spec.sh
 }
 
 # Perform complete build.
 run_all() {
+	set -o errexit # Exit on first error.
 	run_lint
 	run_test
 }
